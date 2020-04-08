4c18777321f5542f0867f86b5ec71b660dea75dd
Yunus Emre Deligöz <ye@deligoz.me>
2020-04-09 00:29:09 +0300

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,12 +8,12 @@
 
             <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
 
-            <p class="text-lg">Give your documentation a boost with Jigsaw. <br class="hidden sm:block">Generate elegant, static docs quickly and easily.</p>
+            <p class="text-lg">Phony is a PHP package that can generate a wide range of fake data.</p>
 
             <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+                <a href="/docs/fake" title="{{ $page->siteName }} fakes" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
 
-                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
+                <a href="https://github.com/phony-php/phony" title="Phony PHP Source Code" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">Source Code</a>
             </div>
         </div>
 
